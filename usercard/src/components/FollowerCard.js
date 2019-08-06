00272255eaa9cb150b9301bbc8234517edc8a3ac
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-06 15:48:53 -0600

    finished followerCard.js, working on some styling

--- usercard/src/components/FollowerCard.js
+++ usercard/src/components/FollowerCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const FollowerCard = ({ userData }) => {
-    console.log("userData from UserCard", userData)
+const FollowerCard = ({ followerData }) => {
+    console.log("followerData from FollowerCard", followerData)
 
     return (
-        <h1>followers</h1>
-        // <div className='userCard'>
-        //     <img src={userData.avatar_url} alt={userData.name} />
-        //     <h2>{userData.name}</h2>
-        //     <h4>{userData.bio}</h4>
-        //     <h4>{userData.location}</h4>
-        // </div>
+        <div>
+            {followerData.map(follower => (
+                <div className='userCard'>
+                    <img src={follower.avatar_url} alt={follower.name} />
+                    <h2>{follower.login}</h2>
+                    <h4>{follower.type}</h4>
+                </div>
+            ))}
+        </div>
+        
     )
     
     
