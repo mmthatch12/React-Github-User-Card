7e93f7e5edff33dc7471d15ab84219140e276f3e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-06 14:53:14 -0600

    finished step 2

--- usercard/src/components/UserCard.js
+++ usercard/src/components/UserCard.js
@@ -4,8 +4,11 @@ const UserCard = ({ userData }) => {
     console.log("userData from UserCard", userData)
 
     return (
-        <div>
+        <div className='userCard'>
             <img src={userData.avatar_url} alt={userData.name} />
+            <h2>{userData.name}</h2>
+            <h4>{userData.bio}</h4>
+            <h4>{userData.location}</h4>
         </div>
     )
     
